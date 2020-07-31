e27f3b9c874b4fefc49fd928f05602510d9b5073
carolinaseg <carolina.s.segovia86@gmail.com>
2020-07-31 12:52:17 -0300

    nuevo comit

---
+++ src/nuevacarpeta.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class nuevacarpeta extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
