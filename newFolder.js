6cc14e20fbc46be04a6963e546a17cafd7a3f1c8
carolinaseg <carolina.s.segovia86@gmail.com>
2020-07-31 11:59:36 -0300

    newFolder add

---
+++ newFolder.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class newFolder extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
